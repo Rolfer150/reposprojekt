e7193f766da0167f93a779374e2c6617264a71f6
rbank <rbanka221@gmail.com>
2021-11-21 15:41:37 +0100

    Projekt - pogoda v3

--- app/src/main/java/com/example/aplikacjatreningowa/MainActivity4.java
+++ app/src/main/java/com/example/aplikacjatreningowa/MainActivity4.java
@@ -52,7 +52,7 @@ public class MainActivity4 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main4);
 
         weatherState = findViewById(R.id.weatherCondition);
         Temperature = findViewById(R.id.temperature);
